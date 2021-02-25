e06d3e66ea997dbb5a55db3b4d9d0cdeebf44da1
Sergio Benitez <sb@sergio.bz>
2021-02-24 17:13:30 -0800

    Document 'AsUncased' trait.

--- src/as_uncased.rs
+++ src/as_uncased.rs
@@ -1,7 +1,19 @@
 use crate::UncasedStr;
 
 /// Helper trait to convert string-like references to `&UncasedStr`.
+///
+/// # Example
+///
+/// ```rust
+/// use uncased::AsUncased;
+///
+/// let string = "Hello, world!".as_uncased();
+/// assert_eq!(string, "hello, world!");
+/// assert_eq!(string, "HELLO, world!");
+/// assert_eq!(string, "HELLO, WORLD!");
+/// ```
 pub trait AsUncased {
+    /// Convert `self` to an [`UncasedStr`].
     fn as_uncased(&self) -> &UncasedStr;
 }
 
